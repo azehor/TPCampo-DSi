dea8023803ec1b12932a0db61cb214519c59ec00
nahuelFredes <nahuelf01@hotmail.com>
2026-02-22 00:10:49 -0300

    feat: Integrar ThemeProvider y configurar tema en el componente principal

--- sgmi_frontend/src/main.tsx
+++ sgmi_frontend/src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { esES as coreEsES } from '@mui/material/locale'
+import { esES as dataGridEsES } from '@mui/x-data-grid/locales'
+
+const theme = createTheme({}, coreEsES, dataGridEsES)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
